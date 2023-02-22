3681b2e833144a582586a9f181a472183d494f31
Joan Falco-Roget <plgjoanfr97@login01.ares.cyfronet.pl>
2023-02-22 13:32:48 +0100

    experiment added

---
+++ experiment.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+
+jobs="10"
+split="100"
+skip="10"
+
+length=$1
+subj=$2
+runs="20"
+surrogates="100"
+rois = -1
+
+# Netsim
+data_dir="Datasets/Netsim/Sim-15/Timeseries"
+results_dir="Results_Netsim-Sim-15_Split-"$split"_Length-"$length
+python main_RCCausality.py $data_dir -rf $results_dir -j $jobs --split $split --skip $skip --length $length --subjects $subj --rois $rois --num_surrogates $surrogates --runs $runs fmri
+
+# Logistic
+#rois="1 2"
+#python main_RCCausality.py $data_dir -rf $results_dir -j $jobs --split $split --skip $skip --length $length --subjects $subj --rois $rois --num_surrogates $surrogates --runs $runs logistic --generate --lags_x2y 2 --lags_y2x 9 --c_x2y 0.4 --c_y2x 0.05 --samples 20 --noise 0.1 1
