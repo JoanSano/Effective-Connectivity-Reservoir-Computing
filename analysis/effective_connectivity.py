b166972484f1925cde2e18d8f5654a477b0f11a8
joan <j.roget@sanoscience.org>
2024-01-17 16:37:24 +0100

    Reorganization architecture. Added network reconstruction

---
+++ analysis/effective_connectivity.py
@@ -0,0 +1,166 @@
+import os
+import numpy as  np
+import networkx as nx
+
+# Relative imports
+from analysis.utils import process_subject_summary
+from analysis.plotting import plot_evidence
+
+class Subject_Effective_Connectivity():
+    def __init__(self, directory, name_subject, ROI_Labels=None) -> None:
+        # Paths
+        self.name_subject = name_subject
+        self.output_dir_subject = os.path.join(directory,name_subject)
+        self.numerical = os.path.join(self.output_dir_subject,"Numerical")
+        self.figures = os.path.join(self.output_dir_subject,"Figures")
+
+        # ROI labels if present
+        if ROI_Labels is not None:
+            self.ROI_Labels = ROI_Labels
+            self.labels = True
+        else:
+            self.labels = False
+
+        # Process summary
+        self.results_subject, self.header, ROI_CODE_Converter = process_subject_summary(
+            directory, name_subject
+        )
+
+        # Deal with ROIs and edge notation. IMPORTANT: ROIs are already sorted from the method itself.
+        # Files with be created in ascending order
+        # In contrast, edges might be reffered by it's index or by its number in the original dataset.
+        # This needs to be dealt with carefully. 
+        self._0IndexedNode_TO_ROI = ROI_CODE_Converter["0IndexedNode_TO_ROI"]
+        self._0Indexed_TO_fileID_pairwise_keys = ROI_CODE_Converter["0Indexed_TO_fileID_pairwise_keys"]
+        self._0Indexed_TO_Original_pariwise_keys = ROI_CODE_Converter["0Indexed_TO_Original_pariwise_keys"]
+        self._0Indexed_ROIs = list(self._0IndexedNode_TO_ROI.keys())
+        self.ROIs = list(self._0IndexedNode_TO_ROI.values())
+
+        # Network and method properties
+        self.N_rois, self.N_edges = self._0Indexed_ROIs[-1]+1, len(self.results_subject)
+        self.lags =  self.results_subject[0][:,0]
+        self.key2lag = dict(zip(self.lags, range(0,len(self.lags))))
+    
+    def __get_file_ID(self, roi_i, roi_j):
+        roi_i, roi_j = sorted([roi_i, roi_j])
+        try:
+            _0indexed_pair = self._0Indexed_TO_Original_pariwise_keys[(roi_i,roi_j)]
+            pair_ID = self._0Indexed_TO_fileID_pairwise_keys[_0indexed_pair]
+            return pair_ID
+        except:
+            raise ValueError(f"No data for the pair of input ROIs ({roi_i},{roi_j})")
+        
+    def __interaction_keys(self, roi_i, roi_j):
+        i2j_key = str(roi_i) + ' --> ' + str(roi_j)
+        j2i_key = str(roi_j) + ' --> ' + str(roi_i)
+        ij_key = str(roi_i) + ' <--> ' + str(roi_j)
+        return i2j_key, j2i_key, ij_key
+
+    def __Network_AT_Lag(self, lag, weighted=False, bidirectional=False):   
+        # Creat adjacency matrix
+        network_lag = np.zeros((self.N_rois,self.N_rois))
+        # Create network
+        G_lag = nx.DiGraph(directed=True)
+        G_lag.add_nodes_from(self.ROIs)
+        # Select the desired lag
+        lag = self.key2lag[lag]
+        # Iterate through 
+        for edge, _0Indexed_edge in self._0Indexed_TO_Original_pariwise_keys.items():
+            file_ID = self._0Indexed_TO_fileID_pairwise_keys[_0Indexed_edge]
+            lag_results = self.results_subject[file_ID][lag,:]
+            edge, _0Indexed_edge = list(edge), list(_0Indexed_edge)
+            # Create edge
+            i2j_key, j2i_key, ij_key = self.__interaction_keys(*edge)
+            if weighted and bidirectional:
+                w = lag_results[self.header["Score "+i2j_key]+self.header["Score "+ij_key]]
+                w_rev = lag_results[self.header["Score "+j2i_key]+self.header["Score "+ij_key]]
+            elif weighted and not bidirectional:
+                w = lag_results[self.header["Score "+i2j_key]]
+                w_rev = lag_results[self.header["Score "+j2i_key]]
+            elif not weighted and bidirectional:
+                w = lag_results[self.header["Evidence "+i2j_key]+self.header["Evidence "+ij_key]]
+                w_rev = lag_results[self.header["Evidence "+j2i_key]+self.header["Evidence "+ij_key]]
+            else:                
+                w = lag_results[self.header["Evidence "+i2j_key]]
+                w_rev = lag_results[self.header["Evidence "+j2i_key]]
+            # Add edges
+            if not np.isnan(w):
+                G_lag.add_edge(*edge, weight=w)
+                network_lag[_0Indexed_edge[0],_0Indexed_edge[1]] = w
+            else:
+                network_lag[_0Indexed_edge[0],_0Indexed_edge[1]] = 0
+            if not np.isnan(w_rev):
+                G_lag.add_edge(*edge[::-1], weight=w_rev)
+                network_lag[_0Indexed_edge[1],_0Indexed_edge[0]] = w_rev
+            else:
+                network_lag[_0Indexed_edge[1],_0Indexed_edge[0]] = 0
+        return G_lag, network_lag
+
+    def Lagged_Networks(self, weighted=False, bidirectional=False):
+        Gs, networks = dict(), dict()
+        for i, lag in enumerate(self.lags):
+            Gs[lag], networks[lag] = self.__Network_AT_Lag(lag, weighted=weighted, bidirectional=bidirectional)
+        return Gs, networks
+
+    def plot_interaction_ij(self, roi_i, roi_j, **kwargs):
+        roi_results = self.results_subject[self.__get_file_ID(roi_i, roi_j)]
+        lags = roi_results[:, self.header["time-lags"]]
+        i2j_key, j2i_key, ij_key = self.__interaction_keys(roi_i, roi_j)
+
+        # Mean results
+        mean_i2j = roi_results[:, self.header[i2j_key]]
+        mean_j2i = roi_results[:, self.header[j2i_key]]
+        mean_i2js = roi_results[:, self.header[i2j_key+"Surrogate"]]
+        mean_j2is = roi_results[:, self.header[j2i_key+"Surrogate"]]
+
+        # Standard Errors
+        sem_i2j = roi_results[:, self.header["SEM "+i2j_key]]
+        sem_j2i = roi_results[:, self.header["SEM "+j2i_key]]
+        sem_i2js = roi_results[:, self.header["SEM "+i2j_key+"Surrogate"]]
+        sem_j2is = roi_results[:, self.header["SEM "+j2i_key+"Surrogate"]]
+
+        # Binary evidence
+        evidence_i2j = roi_results[:, self.header["Evidence "+i2j_key]]
+        evidence_j2i = roi_results[:, self.header["Evidence "+j2i_key]]
+        evidence_ij = roi_results[:, self.header["Evidence "+ij_key]]
+
+        # Naming(s)
+        if 'save' in kwargs.keys():
+            format = kwargs["format"] if 'format' in kwargs.keys() else 'png'
+            dpi = kwargs["dpi"] if 'dpi' in kwargs.keys() else 300
+            name_subject_RCC = self.name_subject + '_ROIs-' +str(roi_i) + 'vs' + str(roi_j)
+            name_subject_RCC_figure = os.path.join(self.figures, name_subject_RCC+'.' + format)
+        else:
+            name_subject_RCC_figure = None
+            dpi = None
+
+        if self.labels:
+            i2jlabel = self.ROI_Labels[roi_i] + r"$\rightarrow$" + self.ROI_Labels[roi_j]
+            j2ilabel = self.ROI_Labels[roi_j] + r"$\rightarrow$" + self.ROI_Labels[roi_i]
+            ijlabel = self.ROI_Labels[roi_i] + r"$\leftrightarrow$" + self.ROI_Labels[roi_j]
+            name_roi_i, name_roi_j = self.ROI_Labels[roi_i], self.ROI_Labels[roi_j]
+        else:
+            i2jlabel = str(roi_i) + r"$\rightarrow$" + str(roi_j)
+            j2ilabel = str(roi_j) + r"$\rightarrow$" + str(roi_i)
+            ijlabel = str(roi_i) + r"$\leftrightarrow$" + str(roi_j)            
+            name_roi_i, name_roi_j = str(roi_i), str(roi_j)
+
+        # Plotting
+        x_label = kwargs["x_label"] if 'x_label' in kwargs.keys() else r"$\tau$"+"(steps)"
+        title = kwargs["title"] if 'title' in kwargs.keys() else None
+        plot_evidence(
+            lags,
+            {"data": mean_i2j, "error": sem_i2j, "label": r"$\rho_{\tau}$"+f"({name_roi_i},{name_roi_j})", "color": "darkorange", "style": "-", "linewidth": 1, "alpha": 1}, 
+            {"data": mean_j2i, "error": sem_j2i, "label": r"$\rho_{\tau}$"+f"({name_roi_j},{name_roi_i})", "color": "green", "style": "-", "linewidth": 1, "alpha": 1}, 
+            {"data": mean_i2js, "error": sem_i2js, "label": r"$\rho_{\tau}$"+f"({name_roi_i},{name_roi_j}"+r"$_{S}$"+")", "color": "bisque", "style": "-", "linewidth": 0.7, "alpha": 0.5}, 
+            {"data": mean_j2is, "error": sem_j2is, "label": r"$\rho_{\tau}$"+f"({name_roi_j},{name_roi_i}"+r"$_{S}$"+")", "color": "lightgreen", "style": "-", "linewidth": 0.7, "alpha": 0.5}, 
+            title=title , y_label="Eff. connectivity scoring", x_label=x_label, limits=(0,1), #scale=0.720, 
+            significance_marks=[
+                {"data": evidence_i2j, "color": "blue", "label": i2jlabel},
+                {"data": evidence_j2i, "color": "red", "label": j2ilabel},
+                {"data": evidence_ij, "color": "purple", "label": ijlabel}
+            ],
+            save=name_subject_RCC_figure, dpi=dpi
+        ) 
+if __name__ == '__main__':
+    pass
---
+++ analysis/utils.py
@@ -0,0 +1,188 @@
+import matplotlib.pylab as plt
+import numpy as np
+import os
+from glob import glob
+import pandas as pd 
+import json
+
+# Relative imports
+from analysis.plotting import plot_evidence
+
+def generate_report(
+        directory, name_subject, roi_i, roi_j,
+        lags, i2j, j2i, surrogate_i2j, surrogate_j2i,
+        Score_i2j, Score_j2i, Score_ij, 
+        evidence_i2j, evidence_j2i, evidence_ij,
+        plot=[True, 'svg']
+    ):   
+    """
+    Saves a summary file with the following column names and info. The format is .tsv
+                    column name                |||  Index value
+                    ===========================================
+                    time-lags                   0
+                    Score 1 <--> 2              1
+                    Score 1 --> 2               2
+                    Score 2 --> 1               3
+                    Evidence 1 <--> 2           4
+                    Evidence 1 --> 2            5
+                    Evidence 2 --> 1            6
+                    1 --> 2                     7
+                    2 --> 1                     8
+                    SEM 1 --> 2                 9
+                    SEM 2 --> 1                 10
+                    1 --> 2Surrogate            11
+                    2 --> 1Surrogate            12
+                    1 --> 2Surrogate SEM        13
+                    2 --> 1Surrogate SEM        14
+    It can also plot and/or save the results in a comprehensive visual summary.
+
+    Arguments
+    ---------
+    lags:
+    
+    Returns
+    ---------    
+    """         
+    # Means and Standard Errors of the Mean
+    mean_i2j, sem_i2j = np.mean(i2j, axis=1), np.std(i2j, axis=1) / np.sqrt(i2j.shape[1])
+    mean_j2i, sem_j2i = np.mean(j2i, axis=1), np.std(j2i, axis=1) / np.sqrt(j2i.shape[1])
+    mean_i2js, sem_i2js = np.mean(surrogate_i2j, axis=1), np.std(surrogate_i2j, axis=1) / np.sqrt(surrogate_i2j.shape[1])
+    mean_j2is, sem_j2is = np.mean(surrogate_j2i, axis=1), np.std(surrogate_j2i, axis=1) / np.sqrt(surrogate_j2i.shape[1])
+
+    # Destination directories and names of outputs
+    output_dir_subject = os.path.join(directory,name_subject)
+    numerical = os.path.join(output_dir_subject,"Numerical")
+    figures = os.path.join(output_dir_subject,"Figures")
+    if not os.path.exists(output_dir_subject):
+        os.mkdir(output_dir_subject)
+    if not os.path.exists(numerical):
+        os.mkdir(numerical)
+    if not os.path.exists(figures):
+        os.mkdir(figures)
+    plot, format = plot
+    name_subject_RCC = name_subject + '_ROIs-' +str(roi_i+1) + 'vs' + str(roi_j+1)
+    name_subject_RCC_figure = os.path.join(figures, name_subject_RCC+'.' + format)
+    name_subject_RCC_numerical = os.path.join(numerical ,name_subject_RCC+'.tsv')
+
+    # Save numerical results
+    i2jlabel, j2ilabel = str(roi_i+1) + ' --> ' + str(roi_j+1), str(roi_j+1) + ' --> ' + str(roi_i+1)
+    ijlabel = str(roi_i+1) + ' <--> ' + str(roi_j+1)
+    results = pd.DataFrame({
+        "time-lags": lags,
+        "Score " + ijlabel: Score_ij,
+        "Score " + i2jlabel: Score_i2j,
+        "Score " + j2ilabel: Score_j2i,
+        "Evidence " + ijlabel: evidence_ij,
+        "Evidence " + i2jlabel: evidence_i2j,
+        "Evidence " + j2ilabel: evidence_j2i,
+        i2jlabel: mean_i2j,
+        j2ilabel: mean_j2i,
+        'SEM ' + i2jlabel: sem_i2j,
+        'SEM ' + j2ilabel: sem_j2i,
+        i2jlabel + 'Surrogate': mean_i2js,
+        j2ilabel + 'Surrogate': mean_j2is,
+        'SEM ' + i2jlabel + 'Surrogate': sem_i2js,
+        'SEM ' + j2ilabel + 'Surrogate': sem_j2is
+    })
+    results.to_csv(name_subject_RCC_numerical, index=False, sep='\t', decimal='.')
+
+    if plot:
+        plot_evidence(
+            lags,
+            {"data": mean_i2j, "error": sem_i2j, "label": r"$\rho_{\tau}$"+f"({str(roi_i+1)},{str(roi_j+1)})", "color": "darkorange", "style": "-", "linewidth": 1, "alpha": 1}, 
+            {"data": mean_j2i, "error": sem_j2i, "label": r"$\rho_{\tau}$"+f"({str(roi_j+1)},{str(roi_i+1)})", "color": "green", "style": "-", "linewidth": 1, "alpha": 1}, 
+            {"data": mean_i2js, "error": sem_i2js, "label": r"$\rho_{\tau}$"+f"({str(roi_i+1)},{str(roi_j+1)}"+r"$_{S}$"+")", "color": "bisque", "style": "-", "linewidth": 0.7, "alpha": 0.5}, 
+            {"data": mean_j2is, "error": sem_j2is, "label": r"$\rho_{\tau}$"+f"({str(roi_j+1)},{str(roi_i+1)}"+r"$_{S}$"+")", "color": "lightgreen", "style": "-", "linewidth": 0.7, "alpha": 0.5}, 
+            save=name_subject_RCC_figure, dpi=300, y_label="Scores", x_label=r"$\tau$"+"(steps)", limits=(0,1), #scale=0.720, 
+            significance_marks=[
+                {"data": evidence_i2j, "color": "blue", "label": i2jlabel},
+                {"data": evidence_j2i, "color": "red", "label": j2ilabel},
+                {"data": evidence_ij, "color": "purple", "label": ijlabel}
+            ]
+        ) 
+
+def process_subject_summary(
+        output_dir, name_subject
+    ):       
+    """
+    The summary file, for each pair of ROIs has the following structure:
+            column name                |||  Index value
+            ===========================================
+            time-lags                   0
+            Score 1 <--> 2              1
+            Score 1 --> 2               2
+            Score 2 --> 1               3
+            Evidence 1 <--> 2           4
+            Evidence 1 --> 2            5
+            Evidence 2 --> 1            6
+            1 --> 2                     7
+            2 --> 1                     8
+            SEM 1 --> 2                 9
+            SEM 2 --> 1                 10
+            1 --> 2Surrogate            11
+            2 --> 1Surrogate            12
+            1 --> 2Surrogate SEM        13
+            2 --> 1Surrogate SEM        14
+    Loads the following summary and stores it in an object with a more convenient, but less compact format.
+
+    Arguments
+    ---------
+    output_dir      (str):
+    name_subject    (str):
+
+    Returns
+    ---------
+    results_subject (numpy array): [pairs, ] containing the database with the results
+    header                 (dict): key-value pairs of the header name and the column ID in results_subject
+    ROI_CODE_Converter     (dict): Contains the codes to move between 0-indexing of ROIs and edges to the
+                                   original numbers (It does not include the label names, only numbers!)
+    """
+
+    # Destination directories and names of outputs
+    output_dir_subject = os.path.join(output_dir,name_subject)
+    numerical = os.path.join(output_dir_subject,"Numerical")
+    
+    # Get files
+    files_interactions = glob(numerical+"/*.tsv")
+    header = {k: v for v,k in enumerate(list(pd.read_csv(files_interactions[0], sep="\t").keys()))}
+    
+    results_subject = np.empty(shape=len(files_interactions), dtype=object)
+    key_pairwise, key_ndoe2roi, key_pairwise_0Indexed, v = {}, {}, {}, 0
+    for i, f in enumerate(files_interactions):
+        # Load summary
+        results_subject[i] = np.genfromtxt(f, delimiter="\t", skip_header=1)
+        # Each file encodes a pairwise interaction
+        pair_rois = f.split("/")[-1].split("_")[-1].split(".")[0].split("-")[-1]
+        ri, rj = int(pair_rois.split("vs")[0]), int(pair_rois.split("vs")[1])
+        # We add ROIs analyzed starting from 0 because the original dataset doesn't need to start from 0!
+        if ri not in key_ndoe2roi.keys():
+            key_ndoe2roi[ri] = v
+            v += 1
+        if rj not in key_ndoe2roi.keys():
+            key_ndoe2roi[rj] = v
+            v += 1
+        # Dictionary with key: value --> 
+        key_pairwise_0Indexed[(key_ndoe2roi[ri],key_ndoe2roi[rj])] = i
+        key_pairwise[(ri,rj)] = (key_ndoe2roi[ri],key_ndoe2roi[rj])
+    
+    # We swap because we want the node id as the key and the real ROI as label to plot!
+    # There are not repeated values, hence we can do it faster and without carefully checking repeated keys
+    key_ndoe2roi = dict([(value, key) for key, value in key_ndoe2roi.items()])
+
+    # Save the code for transparency and other a posteriori plots that the user might want
+    # Convert tuple to a string using a method of your choice
+    deal_with_tuples_keys = lambda original_dict: {'('+','.join(map(str, k))+')': v for k, v in original_dict.items()}
+    deal_with_tuples_value = lambda original_dict: {k: '('+','.join(map(str, v))+')' for k, v in deal_with_tuples_keys(original_dict).items()}
+    ROI_CODE_Converter = {
+        "0IndexedNode_TO_ROI": key_ndoe2roi,
+        "0Indexed_TO_fileID_pairwise_keys": deal_with_tuples_keys(key_pairwise_0Indexed),
+        "0Indexed_TO_Original_pariwise_keys":deal_with_tuples_value(key_pairwise)
+    }
+    with open(f"{output_dir_subject}/Code2Transform_Node2ROIs.json", 'w') as f: 
+        json.dump(ROI_CODE_Converter, f, indent=2)
+    ROI_CODE_Converter = {
+        "0IndexedNode_TO_ROI": key_ndoe2roi,
+        "0Indexed_TO_fileID_pairwise_keys": key_pairwise_0Indexed,
+        "0Indexed_TO_Original_pariwise_keys":key_pairwise
+    }
+    return results_subject, header, ROI_CODE_Converter
