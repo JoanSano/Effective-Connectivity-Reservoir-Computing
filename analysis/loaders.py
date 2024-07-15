8186c21da832b831385f0bc95e295e12835c57e0
joan <j.roget@sanoscience.org>
2024-07-15 15:11:55 +0200

    finished pwCGC -- NETSIM computed

--- analysis/loaders.py
+++ analysis/loaders.py
@@ -106,7 +106,6 @@ class Subject_EC():
         header_interaction = self.headers[pair_ID]
         if np.ndim(roi_results) == 1:
             roi_results = np.expand_dims(roi_results, axis=0)
-            print("Only a single lag has been computed, the info that this plot offers is zero. It should be noted that for conditional GC, this also does not make sense")
         lags = roi_results[:, header_interaction["time-lags"]]
         i2j_key, j2i_key, ij_key = self.__interaction_keys(roi_i, roi_j)
 
